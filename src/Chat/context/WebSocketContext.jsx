fafd327223a58c16a1fb9c3c418152f5f184f64b
Adil-Aadhi <adilshinasmongam@gmail.com>
2026-04-13 13:02:58 +0530

    websocket 2

--- src/Chat/context/WebSocketContext.jsx
+++ src/Chat/context/WebSocketContext.jsx
@@ -37,7 +37,7 @@ const WebSocketProvider = ({ children }) => {
     if (socketRef.current) return;
 
     socketRef.current = new WebSocket(
-     `ws://pccraft3d.duckdns.org/ws/chat/${roomName}/?token=${token}`
+     `wss://pccraft3d.duckdns.org/ws/chat/${roomName}/?token=${token}`
     );
 
     socketRef.current.onopen = () => {
